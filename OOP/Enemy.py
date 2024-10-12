0e64d0bc26e14765ef6545e6947f1352d1252e27
alfan <zahgreat30@gmail.com>
2024-10-12 11:11:53 +0700

    Abstraction in OOP

--- OOP/Enemy.py
+++ OOP/Enemy.py
@@ -2,3 +2,12 @@ class Enemy:
     type_of_enemy: str
     health_points: int = 20
     attack_damage: int = 1
+
+    def talk(self):
+        print(f'I am a {self.type_of_enemy}. Be prepared to fight')
+    
+    def walk_forward(self):
+        print(f'{self.type_of_enemy} move closer to you')
+
+    def attack(self):
+        print(f'{self.type_of_enemy} attacks for {self.attack_damage} damage')
--- OOP/Main.py
+++ OOP/Main.py
@@ -1,7 +1,9 @@
 from Enemy import *
 
-enemy = Enemy()
+zombie = Enemy()
 
-enemy.type_of_enemy = 'Zombie'
+zombie.type_of_enemy = 'Zombie'
 
-print(f"{enemy.type_of_enemy} has {enemy.health_points} and can do attack {enemy.attack_damage}")
+print(zombie.talk())
+print(zombie.walk_forward())
+print(zombie.attack())
