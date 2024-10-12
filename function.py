73908a672f22ee4b15a60e1063f947c9c9f06aa0
alfan <zahgreat30@gmail.com>
2024-10-12 09:58:45 +0700

    add function

--- function.py
+++ function.py
@@ -23,3 +23,18 @@ who_am_i("Alfan")
 print_full_name("Alfan", "Zahriyono")
 print_numbers(firstNumber=20, lastNumber=30)
 print(multiply_numbers(a=20, b=30))
+
+def employee_dictionary(firstName, lastName, age, salary, department, description):
+    create_employee_dictionary = {
+        "firstName": firstName,
+        "lastName": lastName,
+        "age": age,
+        "salary": salary,
+        "department": department,
+        "description": description
+    }
+    return create_employee_dictionary
+
+get_employee_dictionary = employee_dictionary(firstName="Alfan", lastName="Zahriyono", age=25, salary=929340, department="Cyber", description="handsome")
+print(get_employee_dictionary)
+print(get_employee_dictionary.get('firstName'))
