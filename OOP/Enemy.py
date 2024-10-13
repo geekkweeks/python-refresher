1ff1b48832fd6da132e3b80a61ddf0979775be85
alfan <zahgreat30@gmail.com>
2024-10-13 08:14:02 +0700

    Encapsulation concept

--- OOP/Enemy.py
+++ OOP/Enemy.py
@@ -4,15 +4,20 @@ class Enemy:
     "self" refers to the current class/object.
     """
     def __init__(self, type_of_enemy, health_points, attack_damage):
-        self.type_of_enemy = type_of_enemy
+        # by adding __ the property will be set as private(Encapsulation the variable by using private)
+        self.__type_of_enemy = type_of_enemy
+        # these variable below will be set as Public(by default accessible) 
         self.health_points = health_points 
         self.attack_damage = attack_damage 
 
     def talk(self):
-        print(f'I am a {self.type_of_enemy}. Be prepared to fight')
+        print(f'I am a {self.__type_of_enemy}. Be prepared to fight')
     
     def walk_forward(self):
-        print(f'{self.type_of_enemy} move closer to you')
+        print(f'{self.__type_of_enemy} move closer to you')
 
     def attack(self):
-        print(f'{self.type_of_enemy} attacks for {self.attack_damage} damage')
+        print(f'{self.__type_of_enemy} attacks for {self.attack_damage} damage')
+    
+    def get_type_of_enemy(self):
+        return self.__type_of_enemy
--- OOP/Main.py
+++ OOP/Main.py
@@ -3,6 +3,8 @@ from Enemy import *
 zombie = Enemy(type_of_enemy='Small Zombie', health_points=20, attack_damage=100)
 
 
+print(zombie.get_type_of_enemy())
+
 print(zombie.talk())
 print(zombie.walk_forward())
 print(zombie.attack())
